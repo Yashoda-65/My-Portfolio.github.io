7377058417c96cd3505b107a9d8dad7370bba6ff
Yashoda-65 <136297077+Yashoda-65@users.noreply.github.com>
2023-06-12 23:22:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+   ScrollReveal({
+        reset: true,
+        distance: '80px',
+        duration: 2000,
+        delay: 200
+
+    });
+
+    ScrollReveal().reveal('.content, .heading', { origin: 'top' });
+    ScrollReveal().reveal('.services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+    ScrollReveal().reveal('.content h1, .about-img', { origin: 'left' });
+     
+    
+
+
+
+
+
